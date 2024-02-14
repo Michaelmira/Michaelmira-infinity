30c41c334345544da412e5c443d12fad281b6ac4
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-02-14 00:15:24 +0000

    add componets in componets

--- client/src/componets/CustomButton.jsx
+++ client/src/componets/CustomButton.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
+import state from '../store'
+import { snapshot, useSnapshot } from 'valtio'
+
 const CustomButton = ({type, title, customStyles, handleClick }) => {
-    if(type === "filled") {
-        return {
-            backgroundColor: "#000",
-            color: "#fff"
+    const snap = useSnapshot(state);
+    
+    const generateStyle = (type) => {
+        if(type === "filled") {
+            return {
+                backgroundColor: snap.color,
+                color: "#fff"
+            }
         }
     }
 
@@ -12,6 +19,7 @@ const CustomButton = ({type, title, customStyles, handleClick }) => {
     <button
         className={"px-2 py-1.5 flex-1 rounded-md ${customStyles}"}
         style={generateStyle(type)}
+        onClick={handleClick}
     >
         {title}
     </button>
