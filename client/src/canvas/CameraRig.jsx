63e3bcebd9b3e0bfd1b8685c01fa288aae5c5ddc
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-02-14 06:34:05 +0000

    Add useFram to Camerarig

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import { easing } from "maath";
 import { useSnapshot } from "valtio";
@@ -8,7 +8,33 @@ import state from "../store";
 
 const CameraRig = ({children}) => {
     const group = useRef();
-    return <group>{children}</group>
+    const snap = useSnapshot(state);
+
+    useFrame((state, delta) => {
+        const isBreakpoint = window.innerWidth <= 1260;
+        const isMobile = window.innerWidth <=600;
+
+        // set the initial position of the model
+        let targetPosition = [-0.4,0,2];
+        if(snap.intro) {
+            if(isBreakpoint) targetPosition = [0, 0, 2];
+            if(isMobile) targetPosition = [0, 0, 2];
+        }
+
+    
+
+        //  set the model roation smoothly
+        easing.dampE(
+            group.current.rotation,
+            [state.pointer.y / 10, -state.pointer.x / 5, 0],
+            0.25,
+            delta
+        )
+
+    })
+
+
+    return <group ref={group}>{children}</group>
 }
 
 export default CameraRig
