c3cda1359ba1c1c60c20e5da88c1e2bf13059608
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-02-14 00:26:30 +0000

    Add useState to customizer and contents

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -5,13 +5,41 @@ import { useSnapshot } from "valtio";
 import config from "../config/config";
 import state from "../store";
 import { download } from "../assets";
-import {downloadCanvasToImage, reader } from "../config/helpers";
+import { downloadCanvasToImage, reader } from "../config/helpers";
 import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
 import { fadeAnimation, slideAnimation } from "../config/motion";
 
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../componets';
+
 const Customizer = () => {
+  const snap = useSnapshot(state);
+
   return (
-    <div>Customizer</div>
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation("left")}
+          >
+            <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => (
+                  <Tab 
+                    key={tab.name}
+                    tab={tab}\
+                    handleClick={() => {}}
+                  />
+                ))}
+              </div>
+            </div>
+
+
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
   )
 }
 
