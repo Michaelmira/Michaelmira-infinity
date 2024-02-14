840825db9c8bf05a60b592d5daafa315fa67eaf3
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-02-14 00:18:07 +0000

    import and export componets in Index.js

--- client/src/componets/index.js
+++ client/src/componets/index.js
@@ -1,3 +1,13 @@
 import CustomButton from "./CustomButton";
+import AIPicker from "./CustomButton";
+import ColorPicker from "./CustomButton";
+import FilePicker from "./CustomButton";
+import Tab from "./CustomButton";
 
-export { CustomButton };
+export { 
+    CustomButton,
+    AIPicker,
+    ColorPicker,
+    FilePicker,
+    Tab,
+ };
