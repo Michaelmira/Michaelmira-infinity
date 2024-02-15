772f6293ca27fc7f294d82743cf7f351c458ab74
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-02-15 21:53:25 +0000

    Looking for errors in setting up port 8080

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -55,7 +55,7 @@ const Customizer = () => {
     try{
       setGeneratingImg(true);
 
-      const response = await fetch("localhost:8080/api/v1/dalle", {
+      const response = await fetch("http://localhost:8080/api/v1/dalle", {
         method: "POST",
         headers: {
           "Content-Type" : "application/json"
--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,17 +1,18 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
 
 dotenv.config();
 
 const router = express.Router();
 
-const config = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
 
-const openai = new OpenAIApi(config);
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+  });
+
+
 
 router.route('/').get((req, res) => {
     res.status(200).json({ message: "Hello from DALL.E ROUTES" })
@@ -28,7 +29,7 @@ router.route('/').post(async (req, res) => {
             response_format: "b64_json"
         });
 
-        const image = resonse.data.data[0].b64_json;
+        const image = response.data.data[0].b64_json;
 
         res.status(200).json({ photo: image });
     } catch (error) {
